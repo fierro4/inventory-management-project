b9646d265f2a0ab9d9242003fdbd55c844afbe65
fierro4 <priyankasaravanan738@gmail.com>
2025-09-23 18:42:48 +0530

    added images pt2

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -25,7 +25,7 @@ const CardPopularProducts = () => {
               >
                 <div className="flex items-center gap-3">
                   <Image
-                    src={`https://aws-ps-final-s3-inventorymanagement.s3.us-east-1.amazonaws.com/product3.png${
+                    src={`https://aws-ps-final-s3-inventorymanagement.s3.us-east-1.amazonaws.com/product${
                       Math.floor(Math.random() * 3) + 1
                     }.png`}
                     alt={product.name}
